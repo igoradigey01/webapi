ab80d1835c1901e00d33c45476e8d8388104c62d
igoradigey01 <ett66@mail.ru>
2023-07-20 18:18:16 +0300

    chore(shopdb)

--- ShopDB/ShopDbContext.cs
+++ ShopDB/ShopDbContext.cs
@@ -41,7 +41,7 @@ public partial class ShopDbContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseMySql("database=ShopDB;server=localhost;port=3306;userid=root;password=RVOIg*u2a1sp-IdentityX01", Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.33-mysql"));
+        => optionsBuilder.UseMySql("database=ShopDB;server=localhost;port=3306;userid=root;password=RV", Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.33-mysql"));
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
